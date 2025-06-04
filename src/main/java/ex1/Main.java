4800f956aa186efbb5015f26cbbc3d19c1e2b6f9
Администратор <qqq@sdf.df>
2025-06-04 19:35:13 +0400

    start

--- src/main/java/ex1/Main.java
+++ src/main/java/ex1/Main.java
@@ -1,7 +1,22 @@
 package ex1;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Thread st1 = new Thread(new Student("Иван"));
+        st1.start();
+
+        Thread st2 = new Thread(new Student("Дима"));
+        st2.start();
+
+        Thread st3 = new Thread(new Student("Коля"));
+        st3.start();
+
+        st1.join();
+        st2.join();
+        st3.join();
+
 
     }
 }
---
+++ src/main/java/ex1/Student.java
@@ -0,0 +1,22 @@
+package ex1;
+
+public class Student implements Runnable {
+
+    private  final String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name + " взял книгу");
+        try {
+            Thread.sleep((long) (Math.random()*5000));
+            System.out.println(name + " отдал книгу");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
